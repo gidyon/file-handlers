ca578c4f372c113390efd8504a1e0dacc6f53678
Gideon Kamau <gideon.ngk@gmail.com>
2019-10-01 17:14:22 +0300

    test setup and teardown

---
+++ handler/file/file_suite_test.go
@@ -0,0 +1,234 @@
+package file
+
+import (
+	"bytes"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"github.com/onsi/gomega/ghttp"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+)
+
+func TestStatic(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Static Suite")
+}
+
+const (
+	RootDir        = "testdata/"
+	DataDir        = "testdata/data"
+	DefaultFileURL = "/myfile/1"
+)
+
+var (
+	Server           *ghttp.Server
+	Handler          http.Handler
+	DB               *gorm.DB
+	err              error
+	DefaultFileCtype = ""
+)
+
+var _ = BeforeSuite(func() {
+	// start real testing database
+	DB, err = startDB()
+	Expect(err).ShouldNot(HaveOccurred())
+	Expect(DB).ShouldNot(BeNil())
+
+	// call setters
+	SetDefaultDir("uploads")
+	SetMaxUploadSize(10 * 1024 * 1024)
+	SetURLQueryKeyDirectory("d")
+	SetURLQueryKeyFormFile("f")
+	SetURLQueryKeyOwnerID("tag")
+	SetURLQueryKeyOwnerID("id")
+
+	// setup handler
+	Handler, err = NewHandler(&ServerOptions{RootDir, []string{"uploads"}, http.NotFoundHandler(), DB})
+	Expect(err).ShouldNot(HaveOccurred())
+	Expect(Handler).ShouldNot(BeNil())
+
+	// setup server
+	Server = ghttp.NewServer()
+	Server.AppendHandlers(http.HandlerFunc(Handler.ServeHTTP))
+})
+
+var _ = AfterSuite(func() {
+	// tear down server
+	Server.Close()
+
+	// cleanup
+	err = nil
+	err = deleteRootDirFiles()
+	Expect(err).ShouldNot(HaveOccurred())
+})
+
+func deleteRootDirFiles() error {
+	dir := filepath.Join(RootDir, defaultDir)
+	finfos, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	for _, finfo := range finfos {
+		err = os.Remove(filepath.Join(dir, finfo.Name()))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func startDB() (*gorm.DB, error) {
+	param := "charset=utf8&parseTime=true"
+	dsn := "root:hakty11@tcp(localhost:3306)/antibug-files?" + param
+	return gorm.Open("mysql", dsn)
+}
+
+func createFormFile(filename string) (*bytes.Buffer, string, error) {
+	// add file
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, "", err
+	}
+	defer f.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	part, err := writer.CreateFormFile(urlQueryKeyFormFile, filepath.Base(f.Name()))
+	if err != nil {
+		return nil, "", err
+	}
+
+	_, err = io.Copy(part, f)
+	if err != nil {
+		return nil, "", err
+	}
+
+	err = writer.Close()
+	if err != nil {
+		return nil, "", err
+	}
+
+	return body, writer.FormDataContentType(), nil
+}
+
+// Declarations for Ginkgo DSL
+type Done ginkgo.Done
+type Benchmarker ginkgo.Benchmarker
+
+var GinkgoWriter = ginkgo.GinkgoWriter
+var GinkgoRandomSeed = ginkgo.GinkgoRandomSeed
+var GinkgoParallelNode = ginkgo.GinkgoParallelNode
+var GinkgoT = ginkgo.GinkgoT
+var CurrentGinkgoTestDescription = ginkgo.CurrentGinkgoTestDescription
+var RunSpecs = ginkgo.RunSpecs
+var RunSpecsWithDefaultAndCustomReporters = ginkgo.RunSpecsWithDefaultAndCustomReporters
+var RunSpecsWithCustomReporters = ginkgo.RunSpecsWithCustomReporters
+var Skip = ginkgo.Skip
+var Fail = ginkgo.Fail
+var GinkgoRecover = ginkgo.GinkgoRecover
+var Describe = ginkgo.Describe
+var FDescribe = ginkgo.FDescribe
+var PDescribe = ginkgo.PDescribe
+var XDescribe = ginkgo.XDescribe
+var Context = ginkgo.Context
+var FContext = ginkgo.FContext
+var PContext = ginkgo.PContext
+var XContext = ginkgo.XContext
+var When = ginkgo.When
+var FWhen = ginkgo.FWhen
+var PWhen = ginkgo.PWhen
+var XWhen = ginkgo.XWhen
+var It = ginkgo.It
+var FIt = ginkgo.FIt
+var PIt = ginkgo.PIt
+var XIt = ginkgo.XIt
+var Specify = ginkgo.Specify
+var FSpecify = ginkgo.FSpecify
+var PSpecify = ginkgo.PSpecify
+var XSpecify = ginkgo.XSpecify
+var By = ginkgo.By
+var Measure = ginkgo.Measure
+var FMeasure = ginkgo.FMeasure
+var PMeasure = ginkgo.PMeasure
+var XMeasure = ginkgo.XMeasure
+var BeforeSuite = ginkgo.BeforeSuite
+var AfterSuite = ginkgo.AfterSuite
+var SynchronizedBeforeSuite = ginkgo.SynchronizedBeforeSuite
+var SynchronizedAfterSuite = ginkgo.SynchronizedAfterSuite
+var BeforeEach = ginkgo.BeforeEach
+var JustBeforeEach = ginkgo.JustBeforeEach
+var JustAfterEach = ginkgo.JustAfterEach
+var AfterEach = ginkgo.AfterEach
+
+// Declarations for Gomega DSL
+var RegisterFailHandler = gomega.RegisterFailHandler
+var RegisterFailHandlerWithT = gomega.RegisterFailHandlerWithT
+var RegisterTestingT = gomega.RegisterTestingT
+var InterceptGomegaFailures = gomega.InterceptGomegaFailures
+var Ω = gomega.Ω
+var Expect = gomega.Expect
+var ExpectWithOffset = gomega.ExpectWithOffset
+var Eventually = gomega.Eventually
+var EventuallyWithOffset = gomega.EventuallyWithOffset
+var Consistently = gomega.Consistently
+var ConsistentlyWithOffset = gomega.ConsistentlyWithOffset
+var SetDefaultEventuallyTimeout = gomega.SetDefaultEventuallyTimeout
+var SetDefaultEventuallyPollingInterval = gomega.SetDefaultEventuallyPollingInterval
+var SetDefaultConsistentlyDuration = gomega.SetDefaultConsistentlyDuration
+var SetDefaultConsistentlyPollingInterval = gomega.SetDefaultConsistentlyPollingInterval
+var NewWithT = gomega.NewWithT
+var NewGomegaWithT = gomega.NewGomegaWithT
+
+// Declarations for Gomega Matchers
+var Equal = gomega.Equal
+var BeEquivalentTo = gomega.BeEquivalentTo
+var BeIdenticalTo = gomega.BeIdenticalTo
+var BeNil = gomega.BeNil
+var BeTrue = gomega.BeTrue
+var BeFalse = gomega.BeFalse
+var HaveOccurred = gomega.HaveOccurred
+var Succeed = gomega.Succeed
+var MatchError = gomega.MatchError
+var BeClosed = gomega.BeClosed
+var Receive = gomega.Receive
+var BeSent = gomega.BeSent
+var MatchRegexp = gomega.MatchRegexp
+var ContainSubstring = gomega.ContainSubstring
+var HavePrefix = gomega.HavePrefix
+var HaveSuffix = gomega.HaveSuffix
+var MatchJSON = gomega.MatchJSON
+var MatchXML = gomega.MatchXML
+var MatchYAML = gomega.MatchYAML
+var BeEmpty = gomega.BeEmpty
+var HaveLen = gomega.HaveLen
+var HaveCap = gomega.HaveCap
+var BeZero = gomega.BeZero
+var ContainElement = gomega.ContainElement
+var BeElementOf = gomega.BeElementOf
+var ConsistOf = gomega.ConsistOf
+var HaveKey = gomega.HaveKey
+var HaveKeyWithValue = gomega.HaveKeyWithValue
+var BeNumerically = gomega.BeNumerically
+var BeTemporally = gomega.BeTemporally
+var BeAssignableToTypeOf = gomega.BeAssignableToTypeOf
+var Panic = gomega.Panic
+var BeAnExistingFile = gomega.BeAnExistingFile
+var BeARegularFile = gomega.BeARegularFile
+var BeADirectory = gomega.BeADirectory
+var And = gomega.And
+var SatisfyAll = gomega.SatisfyAll
+var Or = gomega.Or
+var SatisfyAny = gomega.SatisfyAny
+var Not = gomega.Not
+var WithTransform = gomega.WithTransform
